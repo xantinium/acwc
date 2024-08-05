bf93f2389ac31ff24ec2a18a7d616de5d14adba0
Chapliy Vladislav <vladislav.chapliy@altcraft.com>
2024-08-05 10:23:16 +0300

    init

--- src/configs/package-json.js
+++ src/configs/package-json.js
@@ -1,8 +1,7 @@
 import path from 'node:path';
-import { CURRENT_DIR } from '../utils.js';
 
 export async function initPackageJSON() {
-    const packageJSON = await import(path.join(CURRENT_DIR, 'package.json'));
+    const packageJSON = await import(path.join(process.execPath, 'package.json'));
 
     console.log(packageJSON);
 }
